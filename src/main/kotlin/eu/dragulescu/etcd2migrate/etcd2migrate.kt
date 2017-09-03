6e2baadda275d6c31e24ce57f2b0080f8f126007
Alex Dragulescu <a.dragulescu@cashpoint.com>
2017-09-03 08:37:55 +0200

    Basic Authentication

--- src/main/kotlin/eu/dragulescu/etcd2migrate/etcd2migrate.kt
+++ src/main/kotlin/eu/dragulescu/etcd2migrate/etcd2migrate.kt
@@ -6,6 +6,7 @@ package eu.dragulescu.etcd2migrate
 import com.google.gson.Gson
 import eu.dragulescu.etcd2.Etcd2Node
 import eu.dragulescu.etcd2.Etcd2Reply
+import khttp.structures.authorization.BasicAuthorization
 import java.lang.String.join
 import java.net.URL
 import java.time.LocalDateTime.now
@@ -13,10 +14,10 @@ import java.time.LocalDateTime.now
 import java.lang.reflect.Type
 import java.util.*
 
-// dir to dir http://127.0.0.1:2379 /dir1/ http://127.0.0.1:2379 /backup/dir1backup/
-// dir to dir http://127.0.0.1:2379 / http://127.0.0.1:2379 /backup/dir1backup/
-// key to key http://127.0.0.1:2379 /k1 http://127.0.0.1:2379 /backup/k22
-// key to key http://127.0.0.1:2379 /k1 http://127.0.0.1:2379 /k22
+// dir to dir http://127.0.0.1:2379 /dir1/ http://127.0.0.1:2379 /backup/dir1backup/ "u1" "p1" "u2" "p2"
+// dir to dir http://127.0.0.1:2379 / http://127.0.0.1:2379 /backup/dir1backup/ "" "" "u2" "p2"
+// key to key http://127.0.0.1:2379 /k1 http://127.0.0.1:2379 /backup/k22 "" "" "" ""
+// key to key http://127.0.0.1:2379 /k1 http://127.0.0.1:2379 /k22 "" "" "" ""
 
 
 fun main(args : Array<String>) {
@@ -31,7 +32,14 @@ fun main(args : Array<String>) {
 class Etcd2ServerOperator(val config: Etcd2ServerConfig) {
     fun get() : Etcd2Node {
         println(now().toString() + ": GET FROM ${config.getFullUrl()}")
-        return khttp.get(config.getFullUrl())
+        if (config.userName.isNotEmpty())
+            return khttp.get(config.getFullUrl(), auth = BasicAuthorization(config.userName, config.userPass))
+                .text
+                .deserializeTo<Etcd2Reply>(Etcd2Reply::class.java)
+                .node
+                .updateSourceIfRequired(config.serverURL)
+        else
+            return khttp.get(config.getFullUrl())
                 .text
                 .deserializeTo<Etcd2Reply>(Etcd2Reply::class.java)
                 .node
@@ -54,7 +62,7 @@ class Etcd2ServerOperator(val config: Etcd2ServerConfig) {
             if (current.dir) {
                 // retrieve dirs if no nodes are already retrieved
                 if (current.source != null)
-                    current = Etcd2ServerOperator(Etcd2ServerConfig(current.source!!, current.key!!)).get()
+                    current = Etcd2ServerOperator(Etcd2ServerConfig(current.source!!, current.key!!, config.userName, config.userPass)).get()
 
                 current.nodes?.forEach { node -> processingQueue.push(node) }
             }
@@ -67,7 +75,10 @@ class Etcd2ServerOperator(val config: Etcd2ServerConfig) {
             var currentData = "value=${current.value}"
             if (current.dir) currentData = "dir=true"
 
-            khttp.put(cpath, headers = mapOf("content-type" to "application/x-www-form-urlencoded"), data = currentData)
+            if (config.userName.isNotEmpty())
+                khttp.put(cpath, headers = mapOf("content-type" to "application/x-www-form-urlencoded"), data = currentData, auth = BasicAuthorization(config.userName, config.userPass))
+            else
+                khttp.put(cpath, headers = mapOf("content-type" to "application/x-www-form-urlencoded"), data = currentData)
         }
     }
 }
@@ -89,7 +100,7 @@ fun <T> String.deserializeTo(t: Type) : T {
 
 fun String.bindTo(server:String) = server.plus(this)
 
-data class Etcd2ServerConfig(val serverURL: URL, val nodeName:String) {
+data class Etcd2ServerConfig(val serverURL: URL, val nodeName:String, val userName:String, val userPass:String) {
     fun getFullUrl() = serverURL.toString().plus(nodeName)
 }
 
@@ -101,11 +112,11 @@ class Etcd2MigrateConfig(args: Array<String>) {
     val destinationServer:Etcd2ServerConfig
 
     init {
-        if (args.size != 4) throw IllegalArgumentException("Required arguments: sourceServer sourceNode destinationServer destinationNode")
+        if (args.size != 8) throw IllegalArgumentException("Required arguments: sourceServer sourceNode destinationServer destinationNode")
         if (args[1].isNullOrBlank() || args[3].isNullOrBlank()) throw IllegalArgumentException("A valid node name must be provided, full path from root required.")
         if (!args[1].startsWith('/') || !args[3].startsWith('/')) throw IllegalArgumentException("All node names must start in root")
 
-        sourceServer = Etcd2ServerConfig(URL(join(URL_SEPARATOR, args[0],ETCD2_PREFIX)), args[1])
-        destinationServer = Etcd2ServerConfig(URL(join(URL_SEPARATOR, args[2],ETCD2_PREFIX)), args[3])
+        sourceServer = Etcd2ServerConfig(URL(join(URL_SEPARATOR, args[0],ETCD2_PREFIX)), args[1], args[4], args[5])
+        destinationServer = Etcd2ServerConfig(URL(join(URL_SEPARATOR, args[2],ETCD2_PREFIX)), args[3], args[6], args[7])
     }
 }
